565fa84464e144d5f78a696645d077c9f5d2a96b
Adrian Pantea <adrianpantea10@gmail.com>
2021-07-02 19:14:25 +0100

    added rotating cube to landpage

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -3,7 +3,20 @@ import React from 'react'
 const LandingPage = () => {
 
   return (
-    <h1>Landing page</h1>
+    <>
+      <body>
+        <div className="wrapper">
+          <div className="cube-area">
+            <div className="box box-front god"></div>
+            <div className="box box-right god"></div>
+            <div className="box box-back god"></div>
+            <div className="box box-left god"></div>
+            <div className="box box-top god"></div>
+            <div className="box box-bottom god"></div>
+          </div>
+        </div>
+      </body>
+    </>
   )
 }
 export default LandingPage
