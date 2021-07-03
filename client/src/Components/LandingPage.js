0e700a73a5fb5172484fd11a86ebc55c068e6c3b
Adrian Pantea <adrianpantea10@gmail.com>
2021-07-03 18:21:53 +0100

    Inserted cube with pictures

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -4,14 +4,20 @@ const LandingPage = () => {
 
   return (
     <>
-      <body>
+      <body className="landingPage-body">
         <div className="wrapper">
           <div className="cube-area">
+            <div className="second-box second-box-front"></div>
             <div className="box box-front god"></div>
+            <div className="second-box second-box-right"></div>
             <div className="box box-right god"></div>
+            <div className="second-box second-box-back"></div>
             <div className="box box-back god"></div>
+            <div className="second-box second-box-left"></div>
             <div className="box box-left god"></div>
+            <div className="second-box second-box-top"></div>
             <div className="box box-top god"></div>
+            <div className="second-box second-box-bottom"></div>
             <div className="box box-bottom god"></div>
           </div>
         </div>
