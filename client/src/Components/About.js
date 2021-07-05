f68f3220fecf6389a3a5e54cffe08554e3bae868
Adrian Pantea <adrianpantea10@gmail.com>
2021-07-05 09:53:04 +0100

    refactored about page with less code.

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,89 +1,88 @@
 import React from 'react'
+import Tilt from 'react-tilt'
+
 import Adrian from '../Images/Developers/Adrian.jpeg'
 import Vanja from '../Images/Developers/Vanja.jpeg'
 import Richard from '../Images/Developers/Richard.png'
-import Tilt from 'react-tilt'
 
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
+library.add(fas, fab)
 
 const About = () => {
 
+  const creators = [
+    {
+      name: 'Adrian Pantea',
+      image: Adrian,
+      content: 'Junior Full-Stack Developer',
+      social: {
+        twitter: 'https://twitter.com/AdrianPantea4',
+        linkedin: 'https://www.linkedin.com/in/adrian-pantea-23391b9a/',
+        github: 'https://github.com/adrianp2021',
+      },
+    },
+    {
+      name: 'Vanja Tominc',
+      image: Vanja,
+      content: 'Junior Full-Stack Developer',
+      social: {
+        linkedin: 'https://www.linkedin.com/in/vanja-tominc-28286b210/',
+        github: 'https://github.com/VaniaTominc',
+      },
+    },
+    {
+      name: 'Richard Knight',
+      image: Richard,
+      content: 'Junior Full-Stack Developer',
+      social: {
+        linkedin: 'https://www.linkedin.com/in/richard-knight-0094ba91/',
+        github: 'https://github.com/RichardKnight88',
+      },
+    }
+  ]
+
   return (
-    <>
-      <body className="aboutPage-body">
-        <div className="container-about">
-          <div className="card-about">
-            <div className="content-about">
-              <div className="imgBx-about"><img src={Adrian} /></div>
-              <div className="contentBx-about">
-                <h3>Adrian Pantea<br /><span>Junior Full-Stack Software Engineer</span></h3>
-              </div>
-            </div>
-            <ul className="sci-about">
-              <li styles="--i:1">
-                <a href="https://www.linkedin.com/in/adrian-pantea-23391b9a/"><i className="fa fa-linkedin-square linkedin"
-                  aria-hidden="true"></i></a>
-              </li>
-              <li styles="--i:2">
-                <a href="https://github.com/adrianp2021"><i className="fa fa-github github" aria-hidden="true"></i></a>
-              </li>
-              <li styles="--i:3">
-                <a href="https://twitter.com/AdrianPantea4"><i className="fa fa-twitter twitter" aria-hidden="true"></i></a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="card-about">
-            <div className="content-about">
-              <div className="imgBx-about"><img src={Vanja} /></div>
-              <div className="contentBx-about">
-                <h3>Vanja Tominc<br /><span>Junior Full-Stack Software Engineer</span></h3>
-              </div>
-            </div>
-            <ul className="sci-about">
-              <li styles="--i:1">
-                <a href="https://www.linkedin.com/in/vanja-tominc-28286b210/"><i className="fa fa-linkedin-square linkedin"
-                  aria-hidden="true"></i></a>
-              </li>
-              <li styles="--i:2">
-                <a href="https://github.com/VaniaTominc"><i className="fa fa-github github" aria-hidden="true"></i></a>
-              </li>
-              <li styles="--i:3">
-                <a href="#"><i className="fa fa-twitter twitter" aria-hidden="true"></i></a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="card-about">
-            <div className="content-about">
-              <div className="imgBx-about"><img src={Richard} /></div>
-              <div className="contentBx-about">
-                <div data-tilt>
-                  <h3>Richard Knight<br /><span>Junior Full-Stack Software Engineer</span></h3>
+    <section className="aboutpage-body">
+      <div className="container-about">
+        {creators.map((creator, i) => {
+          return (
+            <Tilt className="Tilt card-about-wrapper" key={i} options={{ max: 25 }} style={{ height: 250, width: 250 }} >
+              <div className="Tilt-inner">
+                <div className="card-about">
+                  <div className="content-about">
+                    <div className="imgBx-about"><img src={creator.image} /></div>
+                    <div className="contentBx-about">
+                      <h3>{creator.name}<br /><span>{creator.content}</span></h3>
+                    </div>
+                  </div>
+                  <ul className="sci-about">
+                    <li styles="--i:1">
+                      <a className="linkedin" href={creator.social.linkedin} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                    </li>
+                    <li styles="--i:2">
+                      <a className="linkedin" href={creator.social.github} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+                    </li>
+                    <li styles="--i:3">
+                      <a className="twitter" href={creator.social.twitter} rel="noreferrer" target="_blank"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+                    </li>
+                  </ul>
                 </div>
               </div>
-            </div>
-            <ul className="sci-about">
-              <li styles="--i:1">
-                <a href="https://www.linkedin.com/in/richard-knight-0094ba91/"><i className="fa fa-linkedin-square linkedin"
-                  aria-hidden="true"></i></a>
-              </li>
-              <li styles="--i:2">
-                <a href="https://github.com/RichardKnight88"><i className="fa fa-github github" aria-hidden="true"></i></a>
-              </li>
-              <li styles="--i:3">
-                <a href="#"><i className="fa fa-twitter twitter" aria-hidden="true"></i></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </body>
-      <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 250, width: 250 }} >
-        <div className="Tilt-inner"> 👽 </div>
-      </Tilt>
-    </>
+            </Tilt>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 export default About
+
+
+
+
