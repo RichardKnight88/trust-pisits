6daffff948f01109c344e476b5dba8d48d83ae39
richardknight <rknightdesign@gmail.com>
2021-07-08 23:08:31 +0100

    profile pic now initials

--- client/src/Components/ProfilePage.js
+++ client/src/Components/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link, useHistory } from 'react-router-dom'
-import { getCurrentUser, getTokenFromStorage } from './Authentification/auth'
+import { getCurrentUser, getTokenFromStorage, getTwoLetters } from './Authentification/auth'
 import { Rating, Icon } from 'semantic-ui-react'
 import { definedRating } from '../sematinicElements/ratings.js'
 
@@ -64,7 +64,9 @@ const ProfilePage = () => {
                     ?
                     <img className="profile-pic" src={currentUser.profilePicture} alt="blank avatar" />
                     :
-                    <img className="profile-pic" src="https://i.ibb.co/fHJphxZ/Comment-Picture.png" alt="blank avatar" />
+                    <div className='commentator-pic-background'>
+                      <h2 className='engraved'>{getTwoLetters(currentUser.username)}</h2>
+                    </div>
                   }
 
                 </div>
@@ -115,7 +117,9 @@ const ProfilePage = () => {
                                   ?
                                   <img className="profile-pic-in-card" src={currentUser.profilePicture} alt="blank avatar" />
                                   :
-                                  <img className="profile-pic-in-card" src="https://i.ibb.co/fHJphxZ/Comment-Picture.png" alt="blank avatar" />
+                                  <div className='commentator-pic-background'>
+                                    <h2 className='engraved'>{getTwoLetters(currentUser.username)}</h2>
+                                  </div>
                                 }
 
                                 <div className="review-header-username">{item.ownerUsername}</div>
@@ -163,7 +167,8 @@ const ProfilePage = () => {
                                 if (window.confirm('Are you sure you want to delete?')) {
                                   await axios.delete(`/api/gods/${item.placeholderAboutGod}/comments/${item._id}`, {
                                     headers: {
-                                      Authorization: `Bearer ${getTokenFromStorage()}` },
+                                      Authorization: `Bearer ${getTokenFromStorage()}`
+                                    },
                                   })
                                   history.go(0)
                                 }
